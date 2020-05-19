2b28f07940331ce64691f986b29baa8e38368963
AngryUm <eomsh0529@naver.com>
2020-05-19 21:26:32 +0900

    2020-05-19 category.php, shop.tail.php (1)

--- src/theme/basic/mobile/shop/category.php
+++ src/theme/basic/mobile/shop/category.php
@@ -40,7 +40,7 @@ $mshop_categories = get_shop_category_array(true);
                     $j=0;
                     foreach($cate1 as $key=>$cate2){
                         if( empty($cate2) || $key === 'text' ) continue;
-                        
+
                         $mshop_ca_row2 = $cate2['text'];
                         if($j == 0)
                             echo '<ul class="sub_cate sub_cate1">'.PHP_EOL;
@@ -51,11 +51,11 @@ $mshop_categories = get_shop_category_array(true);
                             $mshop_ca_res3 = sql_query(get_mshop_category($mshop_ca_row2['ca_id'], 6));
                             if( count($cate2) > 1 )
                                 echo '<button type="button" class="sub_ct_toggle ct_op">'.get_text($mshop_ca_row2['ca_name']).' 하위분류 열기</button>'.PHP_EOL;
-                            
+
                             $k = 0;
                             foreach($cate2 as $cate3_key=>$cate3){
                                 if( empty($cate2) || $cate3_key === 'text' ) continue;
-                                
+
                                 $mshop_ca_row3 = $cate3['text'];
                                 if($k == 0)
                                     echo '<ul class="sub_cate sub_cate2">'.PHP_EOL;
@@ -110,7 +110,6 @@ $mshop_categories = get_shop_category_array(true);
                     <?php
                     $j++;
                     }
-
                     if($j > 0)
                         echo '</ul>'.PHP_EOL;
                     ?>
@@ -125,17 +124,8 @@ $mshop_categories = get_shop_category_array(true);
                 echo '<p>등록된 분류가 없습니다.</p>'.PHP_EOL;
             ?>
         </div>
-       
-        <?php include(G5_MSHOP_SKIN_PATH.'/boxtodayview.skin.php'); // 오늘 본 상품 ?>
 
-        <ul id="cate_tnb">
-            <li><a href="<?php echo G5_SHOP_URL; ?>/couponzone.php"><i class="fa fa-ticket"></i> 쿠폰존</a></li>
-            <li><a href="<?php echo G5_SHOP_URL; ?>/itemuselist.php"><i class="fa fa-camera"></i> 사용후기</a></li>
-            <li><a href="<?php echo G5_BBS_URL; ?>/faq.php"><i class="fa fa-question"></i>FAQ</a></li>
-            <li><a href="<?php echo G5_BBS_URL; ?>/qalist.php"><i class="fa fa-comments"></i>1:1문의</a></li>
-            <li><a href="<?php echo G5_SHOP_URL; ?>/personalpay.php"><i class="fa fa-credit-card"></i>개인결제</a></li>
-            <li><a href="<?php echo G5_URL; ?>"><i class="fa fa-home"></i>커뮤니티</a></li>
-        </ul> 
+        <?php include(G5_MSHOP_SKIN_PATH.'/boxtodayview.skin.php'); // 오늘 본 상품 ?>
     </div>
 </div>
 <script>
